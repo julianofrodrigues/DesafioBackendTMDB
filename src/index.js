db2bd57084fa713810e519078add82567ef8d1d9
Juliano Ferreira Rodrigues <46505899+julianofrodrigues@users.noreply.github.com>
2022-01-31 17:26:05 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,5 @@ app.use(cors());
 app.use(express.json());
 app.use(moviesRoutes);
 
-app.listen(3333, () => {
-  console.log('Server running');
-});
+app.listen(process.env.PORT || 3333)
+console.log('running!')
